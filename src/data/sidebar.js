16e40e6dc6abb90418a0dd56bb758756c5ffebf2
YoucefElBoukhari <121828759+YoucefElBoukhari@users.noreply.github.com>
2023-11-25 22:45:27 +0100

    Update sidebar.js

--- src/data/sidebar.js
+++ src/data/sidebar.js
@@ -1,9 +1,7 @@
-import { FaTh } from "react-icons/fa";
+import { FaTh,FaStreetView } from "react-icons/fa";
 import { BiImageAdd } from "react-icons/bi";
 import { MdOutlineInventory } from "react-icons/md";
-
-
-
+import { FaRegAddressBook } from "react-icons/fa";
 
 const menu = [
   {
@@ -12,15 +10,15 @@ const menu = [
     path: "/dashboard",
   },
   {
-    title: "Ajouter un produit",
-    icon: <BiImageAdd />,
+    title: "Ajouter un client",
+    icon: <FaRegAddressBook />,
     path: "/add-product",
   },
   {
     title: " Les factures",
     icon: <MdOutlineInventory />,
     path: "/add-client",
-  }
+  },
 ];
 
 export default menu;
